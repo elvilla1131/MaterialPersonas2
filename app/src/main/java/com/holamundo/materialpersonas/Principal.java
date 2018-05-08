c4bb668b62071dbb5763f05f14bbb299c177ffc8
Jose <joseeduardovillanuevaferrer@gmail.com>
2018-05-07 19:49:05 -0500

    Guardado temporal sin validacion

--- app/src/main/java/com/holamundo/materialpersonas/Principal.java
+++ app/src/main/java/com/holamundo/materialpersonas/Principal.java
@@ -30,10 +30,8 @@ public class Principal extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         lstOpciones = findViewById(R.id.lstOpciones);
-        personas = new ArrayList();
-        personas.add(new Persona(R.drawable.images,"1140439156","Raul","Castro",1));
-        personas.add(new Persona(R.drawable.images2,"1140439157","Manuel","Perez",1));
-        personas.add(new Persona(R.drawable.images3,"1140439456","Sergio","Ramos",1));
+        personas = Datos.obtener();
+
 
         llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
@@ -44,8 +42,12 @@ public class Principal extends AppCompatActivity {
     }
 
     public void crearPersonas(View v){
-        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        i = new Intent(Principal.this,CrearPersonas.class);
+        startActivity(i);
+
+
+        //Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+          //      .setAction("Action", null).show();
     }
 
 
